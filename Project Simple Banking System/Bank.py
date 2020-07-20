a8e7bbd4ea3c1f86e485e571c36f77eac9a7873a
Saiful Islam Rasel <raselcse97@gmail.com>
2020-07-20 22:30:55 +0600

    Implement stage 2. Luhan algorithm

--- Project Simple Banking System/Bank.py
+++ Project Simple Banking System/Bank.py
@@ -1,8 +1,9 @@
-import random
+import math
 
 class Bank:
     customer_account = dict()
-    IIN = 400000
+    CARD_NO = 400000000000000
+    PIN_NO = 1000
 
     def __init__(self):
         self.banking_status = True
@@ -10,9 +11,9 @@ class Bank:
     
     def __str__(self):
         if self.customer_login_status:
-            return " 1. Balance \n 2. Log out \n 0. Exit"
+            return "1. Balance\n2. Log out\n0. Exit"
         else:
-            return " 1. Create an account \n 2. Log into account \n 0. Exit"
+            return "1. Create an account\n2. Log into account\n0. Exit"
     def operation(self):
         while self.banking_status:
             print(self)
@@ -31,7 +32,7 @@ class Bank:
                     self.create_account()
                 else:
                     card_no = input('Enter your card number: ')
-                    pin_no = int(input('Enter your PIN: '))
+                    pin_no = input('Enter your PIN: ')
 
                     if self.check_account_validity(card_no, pin_no):
                         self.customer_login_status = True
@@ -41,17 +42,31 @@ class Bank:
         print('Bye!')
 
     def create_account(self):
-        account_number = random.randint(0000000000, 9999999999)
-        card_no = str(Bank.IIN) + str(account_number)
-        pin_no = random.randint(0000, 9999)
+        card_no = str(Bank.CARD_NO)
+        pin_no = str(Bank.PIN_NO)
+
+        Bank.CARD_NO += 1
+        Bank.PIN_NO += 1
+
+        card_no = self.adding_luhan_checksum(card_no)
 
         Bank.customer_account[card_no] = pin_no
-        print(f" Your card has been created \n Your card number: \n {card_no} \n Your card PIN: \n {pin_no}")
+        print(f"Your card has been created\nYour card number:\n{card_no}\nYour card PIN:\n{pin_no}")
     
     def check_account_validity(self, card_no, pin_no):
         if Bank.customer_account.get(card_no, str(-1)) == pin_no:
             return True
         return False
+    
+    def adding_luhan_checksum(self,card_no):
+        bit = [int(x) for x in card_no]
+        bit = [bit[i] * 2 if i % 2 == 0 else bit[i] for i in range(0, len(bit))]
+        bit = [x - 9 if x > 9 else x for x in bit]
+
+        sumation = sum(bit)
+        checksum = (math.ceil(sumation / 10) * 10) - sumation
+                
+        return (card_no + str(checksum))
 
 if __name__ == '__main__':
     bank = Bank()
