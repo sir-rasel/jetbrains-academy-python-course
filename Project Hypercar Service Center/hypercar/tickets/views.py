c4e1e92582fe0d4640e59d706abb45092c856e58
Saiful Islam Rasel <raselcse97@gmail.com>
2020-09-28 16:13:13 +0600

    Project Hypercar Service Center stage 1 completed

---
+++ Project Hypercar Service Center/hypercar/tickets/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+class WelcomeView(View):    
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tickets/welcome.html')
+
